5f57ae595c66f101a619b36ad9d66cfb33157d72
DMITRIY GRIN <greendmitriy407@gmail.com>
2022-07-29 13:05:26 +0300

    little changes in AssetCheckerResult

--- Editor/AssetCheckerResult.cs
+++ Editor/AssetCheckerResult.cs
@@ -7,44 +7,39 @@ namespace AssetChecker
     public class AssetCheckerResult : EditorWindow
     {
         private Vector2 scrollPosition;
-        private List<Object> list;
+        internal List<Object> list;
 
         public static EditorWindow ShowResult(List<Object> list)
         {
             var window = EditorWindow.GetWindow<AssetCheckerResult>("Asset Checker Result");
-            window.SetContext(list);
+            window.list = list;
             window.minSize = new Vector2(300, 200);
             return window;
         }
 
-        private void SetContext(List<Object> list)
-        {
-            this.list = list;
-        }
-
         private void OnGUI()
         {
-            DrawRefList();
+            if (list != null)
+            {
+                DrawRefList();
+            }
         }
 
         private void DrawRefList()
         {
-            if (list != null)
+            scrollPosition = EditorGUILayout.BeginScrollView(scrollPosition, EditorStyles.helpBox);
+            foreach (var obj in list)
             {
-                scrollPosition = EditorGUILayout.BeginScrollView(scrollPosition, EditorStyles.helpBox);
-                foreach (var obj in list)
+                string path = AssetDatabase.GetAssetPath(obj);
+                GUILayout.BeginHorizontal();
+                GUILayout.Label($"{path}", EditorStyles.boldLabel, GUILayout.Height(25));
+                if (GUILayout.Button($"{obj.GetType().Name}", EditorStyles.textArea, GUILayout.MaxWidth(160), GUILayout.Height(25)))
                 {
-                    string path = AssetDatabase.GetAssetPath(obj);
-                    GUILayout.BeginHorizontal();
-                    GUILayout.Label($"{path}", EditorStyles.boldLabel, GUILayout.Height(25));
-                    if (GUILayout.Button($"{obj.GetType().Name}", EditorStyles.textArea, GUILayout.MaxWidth(160), GUILayout.Height(25)))
-                    {
-                        ProjectWindowUtil.ShowCreatedAsset(obj);
-                    }
-                    GUILayout.EndHorizontal();
+                    ProjectWindowUtil.ShowCreatedAsset(obj);
                 }
-                EditorGUILayout.EndScrollView();
+                GUILayout.EndHorizontal();
             }
+            EditorGUILayout.EndScrollView();
         }
     }
 }
