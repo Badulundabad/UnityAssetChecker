bb5fa2fbe3f37ba11ab9e5dce8275e34a5b3d521
DMITRIY GRIN <greendmitriy407@gmail.com>
2022-08-01 17:01:59 +0300

    added AssetObjectData and SceneObjectData
    - also added sprite for button

---
+++ Editor/AssetObjectData.cs
@@ -0,0 +1,16 @@
+﻿namespace AssetChecker
+{
+    public struct AssetObjectData
+    {
+        public readonly string AssetPath;
+        public readonly string Owner;
+        public readonly string Component;
+
+        public AssetObjectData(string assetPath, string owner, string component)
+        {
+            AssetPath = assetPath;
+            Owner = owner;
+            Component = component;
+        }
+    }
+}
---
+++ Editor/SceneObjectData.cs
@@ -0,0 +1,18 @@
+﻿namespace AssetChecker
+{
+    public struct SceneObjectData
+    {
+        public readonly string SceneName;
+        public readonly string ScenePath;
+        public readonly string Owner;
+        public readonly string Component;
+
+        public SceneObjectData(string sceneName, string scenePath, string owner, string component)
+        {
+            SceneName = sceneName;
+            ScenePath = scenePath;
+            Owner = owner;
+            Component = component;
+        }
+    }
+}
